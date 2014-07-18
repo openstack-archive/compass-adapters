7283866a8a139fec9a4a52c463162b9395570e90
Jerry Zhao <zhaoxinyu@huawei.com>
2014-07-18 10:30:25 -0700

    Increase timeout connect to allow for longer wait time.
    When backend gets extremely busy especially such complex system as
    openstack, increase timeout to allow for longer tcp connnection time
    before the backend is deemed dead.
    Refer to: http://docs.openstack.org/high-availability-guide/content/
    ha-aa-haproxy.html
    
    Change-Id: I03f5937424dc77a94eec0c03a890668a15358e01
    Related-bug: #1344144

--- chef/cookbooks/haproxy/attributes/default.rb
+++ chef/cookbooks/haproxy/attributes/default.rb
@@ -58,7 +58,7 @@ default['haproxy']['pid_file'] = "/var/run/haproxy.pid"
 
 default['haproxy']['defaults_options'] = ["tcpka", "httpchk", "tcplog", "httplog"]
 default['haproxy']['x_forwarded_for'] = false
-default['haproxy']['defaults_timeouts']['connect'] = "5s"
+default['haproxy']['defaults_timeouts']['connect'] = "10s"
 default['haproxy']['defaults_timeouts']['client'] = "100s"
 default['haproxy']['defaults_timeouts']['server'] = "100s"
 default['haproxy']['cookie'] = nil
