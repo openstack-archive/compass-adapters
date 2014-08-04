8c77cce3c5d4084049c280800fc5600df251d8e8
Jerry Zhao <zhaoxinyu@huawei.com>
2014-08-04 16:14:48 -0700

    Associate role related logs in role configs.
    Define role related log attributes in role configs so that
    recipe does not need to check its own role every time when
    writing a role related config from template, because those
    info would already have been in role definition and passed
    down to node attributes.
    Process monitoring can use the same mechanism.
    
    Change-Id: Ifb10da356baf99d2d7e21c8d4d3d8df38fc81f58

--- chef/roles/os-ops-database.rb
+++ chef/roles/os-ops-database.rb
@@ -1,5 +1,15 @@
 name "os-ops-database"
 description "Currently MySQL Server (non-ha)"
+override_attributes(
+  "rsyslog" => {
+    "rhelloglist" => {
+      "mysqld" => "/var/log/mysqld.log"
+    },
+    "debianloglist" => {
+      "mysqld" => "/var/log/mysql.log"
+    }
+  }
+)
 run_list(
   "role[os-base]",
   "recipe[openstack-ops-database::server]",
