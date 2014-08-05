c96ecd1a42908ccdf4414deca7e39b18cc2b5e43
Jerry Zhao <zhaoxinyu@huawei.com>
2014-08-05 12:30:59 -0700

    Add collectd cookbook
    Will associate monitored processes with roles later.
    
    Change-Id: I33a14255a11f52be87c1551a3dd2b4542c11eebc

---
+++ chef/cookbooks/collectd/recipes/client.rb
@@ -0,0 +1,59 @@
+#
+# Cookbook Name:: collectd
+# Recipe:: client
+#
+# Copyright 2010, Atari, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "collectd"
+
+#servers = []
+#search(:node, 'recipes:collectd\\:\\:server') do |n|
+#  servers << n['fqdn']
+#end
+
+#if servers.empty?
+#  raise "No servers found. Please configure at least one node with collectd::server."
+#end
+
+#collectd_plugin "network" do
+#  options :server=>servers
+#end
+
+cookbook_file "#{node['collectd']['plugin_dir']}/kairosdb_writer.py" do
+  source "kairosdb_writer.py"
+  owner "root"
+  group "root"
+  mode 00644
+  notifies :restart, "service[collectd]"
+  action :create_if_missing
+end
+
+node['collectd']['plugins'].each_pair do |plugin_key, options|
+  collectd_plugin plugin_key do
+    options  options
+  end
+end
+
+collectd_python_plugin "kairosdb_writer" do
+  opts  =    {"KairosDBHost"=>node['collectd']['server']['host'],
+              "KairosDBPort"=>node['collectd']['server']['port'],
+              "KairosDBProtocol"=>node['collectd']['server']['protocol'],
+              "LowercaseMetricNames"=>"true",
+              "Tags" => "host=#{node['fqdn']}\" \"role=OSROLE\" \"location=China.Beijing.TsingHua\" \"cluster=#{node['cluster']}",
+              "TypesDB" => node['collectd']['types_db']
+             }
+  options(opts)         
+end
--- chef/roles/test-synclog.rb
+++ chef/roles/test-synclog.rb
@@ -1,5 +1,6 @@
 name "test-synclog"
 description "Sync application related logs for debugging"
 run_list(
-  "recipe[rsyslog::client]"
+  "recipe[rsyslog::client]",
+  "reciple[collectd::client]"
   )
