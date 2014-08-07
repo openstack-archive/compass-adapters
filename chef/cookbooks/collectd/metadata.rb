b579df04b783f99ffb298cc4b4073a05eed5a178
Jerry Zhao <zhaoxinyu@huawei.com>
2014-08-06 21:17:47 -0700

    Cherry pick from master for collectd cookbooks
    Change-Id: Iad37cbb8c3bda54f3873b094824772e6bb98cf65

--- chef/cookbooks/collectd/metadata.rb
+++ chef/cookbooks/collectd/metadata.rb
@@ -7,5 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.3"
 supports         "ubuntu"
 supports         "centos"
-depends          "apt"
 depends          "yum"
