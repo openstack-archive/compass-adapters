96ae6a44d2ad94491d41ac0c6a26bf3af6322a1c
Weidong Shao <weidong.shao@huawei.com>
2014-09-02 23:44:25 +0000

    Fix an error on empty list from chef search
    Change-Id: Iacfd5614849b3885c5be833122d1f640c34ce66c

--- chef/cookbooks/openstack-identity/libraries/default.rb
+++ chef/cookbooks/openstack-identity/libraries/default.rb
@@ -27,7 +27,7 @@ def node_election(role, tag, chef_environment = nil)
     nodes = search(:node, "run_list:role\\[#{role}\\] AND \
                    chef_environment:#{chef_environment}") || []
     nodes = nodes.sort_by { |node| node.name } unless nodes.empty?
-    if node.name.eql?(nodes.first.name)
+    if nodes.empty? or node.name.eql?(nodes.first.name)
       node.tags << tag unless node.tags.include?(tag)
       node.save
     end
