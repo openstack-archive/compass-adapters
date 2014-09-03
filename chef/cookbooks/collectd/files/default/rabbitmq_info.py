891548dfc533e034834dd49809c71e7885517d93
Jerry Zhao <zhaoxinyu@huawei.com>
2014-09-03 15:13:03 -0700

    check if messages key exist
    If it does not exist, skip counting messages but count the values
    of other metrics.
    
    Change-Id: I9c00ad653654107148fdf26e77fd6f6f5d66d6bd

--- chef/cookbooks/collectd/files/default/rabbitmq_info.py
+++ chef/cookbooks/collectd/files/default/rabbitmq_info.py
@@ -85,11 +85,12 @@ def get_stats():
     for i in resp:
         if "messages" in i:
             stats['ctl_messages'] += i['messages']
-            stats['ctl_memory'] += i['memory']
-            stats['ctl_consumers'] += i['consumers']
             stats['ctl_messages_%s' % i['name']] = i['messages']
-            stats['ctl_memory_%s' % i['name']] = i['memory']
-            stats['ctl_consumers_%s' % i['name']] = i['consumers']
+        stats['ctl_memory'] += i['memory']
+        stats['ctl_consumers'] += i['consumers']
+        stats['ctl_messages_%s' % i['name']] = i['messages']
+        stats['ctl_memory_%s' % i['name']] = i['memory']
+        stats['ctl_consumers_%s' % i['name']] = i['consumers']
     if not stats['ctl_memory'] > 0:
         logger('warn', '%s reports 0 memory usage. This is probably incorrect.'
                % RABBITMQ_API)
