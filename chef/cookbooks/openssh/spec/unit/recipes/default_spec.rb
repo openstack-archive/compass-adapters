22bb9f76434cfee29e33e4443ba349d63d9196a5
Weidong Shao <weidong.shao@huawei.com>
2014-09-03 06:47:16 +0000

    Merge openssh cookbook from OpenDeployment
    Note that this merge may break things. The change from OpenDeployment
    is subject to further review at a later time. The change introduce
    openstack specific logic in an otherwise independent openssh recipe.
    
    Change-Id: I568e4d2329a1d0e2670e1b0340afca4dddf4f23a

---
+++ chef/cookbooks/openssh/spec/unit/recipes/default_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe 'openssh::default' do
+  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+
+  it 'installs the openssh packages' do
+    expect(chef_run).to install_package('openssh-client')
+    expect(chef_run).to install_package('openssh-server')
+  end
+
+  it 'starts the ssh service' do
+    expect(chef_run).to start_service('ssh')
+    expect(chef_run).to enable_service('ssh')
+  end
+
+  it 'writes the ssh_config' do
+    template = chef_run.template('/etc/ssh/ssh_config')
+    expect(template).to be
+    expect(template.mode).to eq('0644')
+    expect(template.owner).to eq('root')
+    expect(template.group).to eq('root')
+  end
+
+  describe 'sshd_config' do
+
+    it 'writes the sshd_config' do
+      template = chef_run.template('/etc/ssh/sshd_config')
+      expect(template).to be
+      expect(template.mode).to eq('0644')
+      expect(template.owner).to eq('root')
+      expect(template.group).to eq('root')
+    end
+
+    it 'allow legacy default AuthorizedKeysFile behavior' do
+      expect(chef_run).to_not render_file('/etc/ssh/sshd_config').with_content(/AuthorizedKeysFile.*/)
+    end
+
+    it 'writes a match group block' do
+      chef_run.node.set['openssh']['server']['match'] = { 'Group admins' => { 'permit_tunnel' => 'yes' } }
+      chef_run.converge(described_recipe)
+      expect(chef_run).to render_file('/etc/ssh/sshd_config').with_content(/Match Group admins\n\s\sPermitTunnel yes/)
+    end
+
+    it 'skips match group block' do
+      chef_run.node.set['openssh']['server']['match'] = {}
+      chef_run.converge(described_recipe)
+      expect(chef_run).to_not render_file('/etc/ssh/sshd_config').with_content(/Match Group admins\n\s\sPermitTunnel yes/)
+    end
+
+  end
+end
