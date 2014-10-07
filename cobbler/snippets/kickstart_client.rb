41e47c73111b58000d9e37f1e2f61f3dfd62548a
xiaodongwang <xiaodongwang@huawei.com>
2014-10-06 17:30:47 -0700

    do not set no_proxy if http_proxy is not set
    Change-Id: Id8e0bbed0699d73eaf42778cf5295afd8f0af6b9

--- cobbler/snippets/kickstart_client.rb
+++ cobbler/snippets/kickstart_client.rb
@@ -11,11 +11,11 @@ ENV['http_proxy'] = '$proxy'
 ENV['https_proxy'] = '$proxy'
 ENV['HTTP_PROXY'] = '$proxy'
 ENV['HTTPS_PROXY'] = '$proxy'
-#end if
-#if $getVar('ignore_proxy', '') != ""
+    #if $getVar('ignore_proxy', '') != ""
 no_proxy         '$ignore_proxy'
 ENV['no_proxy'] = '$ignore_proxy'
 ENV['NO_PROXY'] = '$ignore_proxy'
+    #end if
 #end if
 validation_client_name 'chef-validator'
 json_attribs nil
