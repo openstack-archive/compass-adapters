a375b3715352d405505a47af5fa141dd92e1f034
Jerry Zhao <zhaoxinyu@huawei.com>
2014-11-26 22:08:17 -0800

    fix gem install failure for ubuntu
    chef-client gem install does not honor .gemrc in time,
    so specify the http-proxy option explicitly.
    moreover, post-apt-config kickstart file has a syntax
    error, which is also fixed in the patch.
    
    Change-Id: Iadf78b6c0aff691e3f6824c20e906b75b681e848
    Closes-bug: #1393967
    Closes-bug: #1396948

--- chef/cookbooks/mysql/recipes/ruby.rb
+++ chef/cookbooks/mysql/recipes/ruby.rb
@@ -44,8 +44,17 @@ node['mysql']['client']['packages'].each do |name|
   resources("package[#{name}]").run_action(:install)
 end
 
+# unknown reason cause chef-client not to honor .gemrc immediately
+# even not until timeout is reached, so specify the options explicitly.
 if node['local_repo'].nil? or node['local_repo'].empty?
-  chef_gem 'mysql'
+  if node['proxy_url']
+    gem_package 'mysql' do
+      options("--http-proxy #{node['proxy_url']}")
+      action :install
+    end
+  else
+    chef_gem 'mysql'
+  end
 else
   gem_package 'mysql' do
     options("--clear-sources --source #{node['local_repo']}/gem_repo/")
